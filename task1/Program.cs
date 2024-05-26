659b2fa9cedf8779c85cab55fbef26615e48f6f4
BERGAMOT <maksinator.ru@yandex.ru>
2024-05-26 20:12:29 +0300

    сделал строковый массив

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов массива: ");
+
+int dlinaMassiva = int.Parse(Console.ReadLine());
+
+//int[] massiv = new int[dlinaMassiva];
+string[] Array = new string[dlinaMassiva];
+
+for (int i = 0; i < dlinaMassiva; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    Array[i] = Console.ReadLine();
+}
+Console.WriteLine(Array[0]);
+Console.WriteLine(Array[1]);
+Console.WriteLine(Array[2]);
+Console.WriteLine(Array[3]);
+Console.WriteLine(Array[4]);
