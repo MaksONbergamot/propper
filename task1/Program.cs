d9b98193852906d32c8dd3f6139f72acedf3c34b
BERGAMOT <maksinator.ru@yandex.ru>
2024-05-27 00:23:25 +0300

    добавил счётчик правильных ответов

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.Write("Введите количество элементов массива: ");
 
-int dlinaMassiva = int.Parse(Console.ReadLine());
+int dlinaMassiva = Convert.ToInt32(Console.ReadLine());
 
-//int[] massiv = new int[dlinaMassiva];
 string[] Array = new string[dlinaMassiva];
 
 for (int i = 0; i < dlinaMassiva; i++)
@@ -10,8 +9,22 @@ for (int i = 0; i < dlinaMassiva; i++)
     Console.Write("Введите элемент массива: ");
     Array[i] = Console.ReadLine();
 }
-Console.WriteLine(Array[0]);
-Console.WriteLine(Array[1]);
-Console.WriteLine(Array[2]);
-Console.WriteLine(Array[3]);
-Console.WriteLine(Array[4]);
+//проверка массива
+// Console.WriteLine(Array[0]);
+// Console.WriteLine(Array[1]);
+// Console.WriteLine(Array[2]);
+// Console.WriteLine(Array[3]);
+// Console.WriteLine(Array[4]);
+
+int rightAnswers = 0;
+for (int ii = 0; ii < dlinaMassiva; ii++)
+{
+    if (Array[ii].Length <= 3)
+    {
+        rightAnswers++;
+    }
+
+}
+//Console.WriteLine(rightAnswers);
+
+string[] Array2 = new string[rightAnswers];
