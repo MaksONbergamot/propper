7a4d18a73106a7026fe33c77ec8742d8f5737fde
BERGAMOT <maksinator.ru@yandex.ru>
2024-05-27 02:24:03 +0300

    finish

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,7 +6,7 @@ string[] Array = new string[dlinaMassiva];
 
 for (int i = 0; i < dlinaMassiva; i++)
 {
-    Console.Write("Введите элемент массива: ");
+    Console.Write("Введите элемент массива " + i + ": ");
     Array[i] = Console.ReadLine();
 }
 //проверка массива
@@ -25,6 +25,27 @@ for (int ii = 0; ii < dlinaMassiva; ii++)
     }
 
 }
-//Console.WriteLine(rightAnswers);
+//Console.WriteLine("правильных ответов " + rightAnswers); //проверка
 
 string[] Array2 = new string[rightAnswers];
+int n = 0;
+
+for (int schet = 0; schet < rightAnswers; schet++)
+{
+    if (Array[schet].Length <= 3)
+    {
+        Array2[n] = Array[schet];
+        n++;
+    }
+    else 
+    {
+        rightAnswers++;
+    }
+}
+
+for (int schet2 = 0; schet2 < Array2.Length; schet2++)
+{
+Console.Write(Array2[schet2] + ", ");
+
+}
+
